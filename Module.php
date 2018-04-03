6904bc6231e741d61d440411c6efb9016754c7ff
fedornabilkin <fedornabilkin@yandex.ru>
2018-04-03 22:31:25 +0300

    fix translations

--- Module.php
+++ Module.php
@@ -9,12 +9,23 @@
 namespace fedornabilkin\redirect;
 
 
+use yii\i18n\PhpMessageSource;
+
 class Module extends \yii\base\Module {
 
     public $controllerNamespace = 'fedornabilkin\redirect\controllers';
 
     public function init() {
+
         parent::init();
+        if (!isset(\Yii::$app->i18n->translations['redirect*'])) {
+            \Yii::$app->i18n->translations['redirect*'] = [
+                'class' => PhpMessageSource::class,
+                'sourceLanguage' => 'ru',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+
     }
 
 }
