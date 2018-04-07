3cba120209f7aa2deb45b28a009246188a1fd839
fedornabilkin <fedornabilkin@yandex.ru>
2018-04-08 01:32:36 +0300

    add languige

---
+++ messages/ua/redirect.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+
+return [
+    'Save' => 'Зберегти',
+    'Update' => 'Редагувати',
+    'Delete' => 'Видалити',
+    'Remove empty redirects' => 'Видалити порожні перенаправлення',
+    'All records that are not redirected anywhere along with statistics of visits will be deleted. Remove?' => 'Будуть видалені всі записи, які нікуди не перенаправляють разом зі статистикою відвідувань. Видалити?',
+    'Delete failed!' => 'Помилка при видаленні!',
+    'Record has been successfully removed.' => 'Записи успішно видалені.',
+
+    'Are you sure you want to delete this item?' => 'Ви впевнені, що хочете видалити цей елемент?',
+    'Create Redirect' => 'Створити перенаправлення',
+    'Update Redirect' => 'Редагувати перенаправлення',
+
+    'ID' => '',
+    'Visited' => 'Візитів',
+    'From' => 'Звідки',
+    'To' => 'Куди',
+    'Status code' => 'Http код',
+    'Default 301' => 'За замовчуванням 301',
+    'Created At' => 'Створений',
+    'Updated At' => 'Змінений',
+
+    'Redirect' => 'Неіснуючу адресу',
+    'Ip' => 'Айпі',
+    'Agent' => 'Браузер',
+    'Country Code' => 'Код країни',
+    'Referrer' => 'Джерело',
+
+    'Redirects' => 'Перенаправлення',
+    'Create redirect' => 'Створити перенаправлення',
+
+    'List visits' => 'Список відвідувань',
+
+    "You can't send to this address." => 'Неможливо направити на цю адресу.',
+    "Invalid address." => 'Неприпустимий адресу.',
+    "The requested page does not exist." => 'Запитувана сторінка не існує.',
+
+    '{0, date, MMMM dd, YYYY HH:mm}' => '{0, date, dd MMMM, YYYY HH:mm}',
+];
